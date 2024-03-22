7464839625862cf4e5aa4e3552c94c7b253cfa4e
KaiT <107540255+ITPHashagi@users.noreply.github.com>
2024-03-22 17:30:47 +0700

    update

---
+++ lab5/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Object Oriented Programming PHP</title>
+<!-- Unicode Vietnamese -->
+<meta charset="utf-8">
+<meta name="author" content="trendemy.com" />
+<!-- CSS definition file -->
+
+<link href="site.css" rel="stylesheet" />
+</head>
+
+<body>
+<div id="wrapper" class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="header">
+<h2>Project training - Build a sales website</h2>
+</div>
+</div>
+
+</div>
+</div>
+<div id="wrapper" class="container">
+<footer>
+Comeback <a href="/demo/lap3">Home page</a><br /> 2022 - Trendemy
+</footer>
+</div>
+</body>
+</html>
